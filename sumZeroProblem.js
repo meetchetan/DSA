ce5ff98cc4159734dca02f9413be6d71c81f2d40
meetchetan <chetang525@gmail.com>
2023-10-17 23:30:29 +0530

    added time complexity

--- sumZeroProblem.js
+++ sumZeroProblem.js
@@ -16,3 +16,6 @@ let sumPairZero = (array) => {
 
 let res = sumPairZero([-5, -4, -3, -2, 0, 2, 4, 6, 8]);
 console.log(res);
+
+
+// time complexity will be o(n^2), as there is a loop insite a loop
