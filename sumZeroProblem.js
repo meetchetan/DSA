ab0db80d6067a04eec3956cd0b43174a9b8c7e96
meetchetan <chetang525@gmail.com>
2023-10-17 23:28:27 +0530

    DSA problem 1 solved

---
+++ sumZeroProblem.js
@@ -0,0 +1,18 @@
+// Checking sum zero
+//  [-5, -4, -3, -2, 0, 2, 4, 6, 8] => input
+//  [?, ?] => Output [-4, 4]
+
+let sumPairZero = (array) => {
+  //   for (let i = 0; i < array.length; i++) {
+  for (let i of array) {
+    for (let j = 1; j < array.length; j++) {
+      if (i - array[j] == 0) {
+        return [i, array[j]];
+      }
+      break;
+    }
+  }
+};
+
+let res = sumPairZero([-5, -4, -3, -2, 0, 2, 4, 6, 8]);
+console.log(res);
